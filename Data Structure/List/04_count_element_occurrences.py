9645517d5393806a5e0e3dda9237315dff465d26
Hardik Singh <hardiksingh82278@gmail.com>
2026-07-15 10:28:15 +0530

    Data Structure/List/04_count_element_occurrences.py modified

--- Data Structure/List/04_count_element_occurrences.py
+++ Data Structure/List/04_count_element_occurrences.py
@@ -1,4 +1,6 @@
 """Write a program to print the number of occurrences of a specified
 element in a list."""
 odd_num=[1,1,3,3,5,7,7,7,9,9]
-print(odd_num.count(9))
+print(odd_num)
+a=int(input("Enter that odd number from the list whose count you are finding : "))
+print(odd_num.count(a))
